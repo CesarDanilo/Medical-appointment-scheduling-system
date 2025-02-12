d175ef39ece6ef3032879475bfcacb073c1cd384
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-12 15:54:20 -0300

    create migrate users

---
+++ back/src/migrations/20250212183814-users.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'users', {
+
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        validate: {
+          isIn: [['paciente', 'médico', 'admin']]
+        }
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+    });
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+
+  }
+};
