aa74056144a7036645511f17f9574a4bfb268117
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-13 15:21:41 -0300

    realocando arquinos para o destino correto & criando models users

---
+++ back/src/database/models/Users.js
@@ -0,0 +1,51 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Users extends Model {
+        static associate(models) {
+            // Users.hasMany(models.Pedido, { foreignKey: 'usuario_id' });
+        }
+    }
+
+    Users.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            validate: {
+                isIn: [['paciente', 'médico', 'admin']]
+            }
+        },
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
+        }
+    }, {
+        sequelize,
+        modelName: 'Users',
+        tableName: 'users',
+        timestamps: true // Habilita o gerenciamento automático de createdAt e updatedAt
+    });
+
+    return Users;
+};
