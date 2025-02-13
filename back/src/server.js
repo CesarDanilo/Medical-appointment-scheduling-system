44d3ae97c0f96b13482280293543e4ffc2b00c09
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-13 15:30:21 -0300

    created controllers

--- back/src/server.js
+++ back/src/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const pool = require('./config/db');
+const pool = require('./database/config/db');
 
 const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT NOW()');
-    res.send(`Database connection successful. Current time: ${rows[0].now}`);
+    const { rows } = await pool.query('SELECT * from users');
+    res.send(`${rows}`);
   } catch (err) {
     console.error(err);
     res.status(500).send('Database connection failed');
