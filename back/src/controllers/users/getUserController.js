3649c6a71f4a098b2e98b20bcd17feefbd440b6d
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-15 16:44:40 -0300

    corrigindo conexão com o banco de dados agora api getUser funcionando

--- back/src/controllers/users/getUserController.js
+++ back/src/controllers/users/getUserController.js
@@ -3,12 +3,12 @@ const { Users } = require("../../database/models");
 const userControllers = async (req, res) => {
     try {
         const result = await Users.findAll({
-            atributes: ["id", "name", "email", "password", "role", "createdAt", "updatedAt"]
+            attributes: ["id", "name", "email", "password", "role", "createdAt", "updatedAt"] // Corrigido "atributes" para "attributes"
         });
-        return res.status(200).res.json({ data: result });
+        return res.status(200).json({ data: result }); // Corrigido "res.json" para "json"
     } catch (error) {
-        return res.status(400).json({ erro: error.message });
+        return res.status(400).json({ error: error.message }); // Corrigido "erro" para "error" (boa prática)
     }
-}
+};
 
 module.exports = userControllers;
