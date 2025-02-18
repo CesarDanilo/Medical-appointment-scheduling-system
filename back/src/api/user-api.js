32c80b060997100184eda30cba9cba5e502f5b55
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-18 15:49:48 -0300

    created query filter user api

--- back/src/api/user-api.js
+++ back/src/api/user-api.js
@@ -4,5 +4,6 @@ const routes = express.Router();
 const userControllers = require('../controllers/users');
 
 routes.get('/', userControllers.getuser);
+routes.get('/:name', userControllers.getuser);
 
 module.exports = routes;
--- back/src/controllers/users/getUserController.js
+++ back/src/controllers/users/getUserController.js
@@ -1,13 +1,26 @@
 const { Users } = require("../../database/models");
+const { Op } = require("sequelize");
 
 const userControllers = async (req, res) => {
     try {
+        const { name } = req.query;
+        const where = {};
+
+        // CRIANDO UM WHARE PARA TRAZER POR NOME
+        if (name) {
+            where.name = {
+                [Op.iLike]: `%${name}%`
+            }
+        }
+
         const result = await Users.findAll({
-            attributes: ["id", "name", "email", "password", "role", "createdAt", "updatedAt"] // Corrigido "atributes" para "attributes"
+            where,
+            attributes: ["id", "name", "email", "password", "role", "createdAt", "updatedAt"]
         });
-        return res.status(200).json({ data: result }); // Corrigido "res.json" para "json"
+
+        return res.status(200).json({ data: result });
     } catch (error) {
-        return res.status(400).json({ error: error.message }); // Corrigido "erro" para "error" (boa prática)
+        return res.status(400).json({ error: error.message });
     }
 };
 
