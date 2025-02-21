2937c3cfc578560569287b1d778ec0c8271c1e95
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-21 15:10:55 -0300

    ajustando rotas

--- back/src/api/users/user-api.js
+++ back/src/api/users/user-api.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 const userControllers = require('../../controllers/users');
 
 routes.get('/', userControllers.getuser);
-routes.get('/:name', userControllers.getuser);
+routes.get('/name/:name', userControllers.getuser);
 routes.get('/:id', userControllers.getuser);
 
 routes.post('/create-user', userControllers.createUser);
@@ -12,3 +12,5 @@ routes.post('/create-user', userControllers.createUser);
 routes.put('/:id', userControllers.updateUser);
 
 module.exports = routes;
+
+
--- back/src/controllers/users/updateUserController.js
+++ back/src/controllers/users/updateUserController.js
@@ -1,22 +1,38 @@
 const { Users } = require("../../database/models");
 
-const updateUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const data = req.body;
+const updateUser = async (req, res, next) => {
+    const { id } = req.params;
 
+    try {
+        const dados = req.body;
+
+        // Tentar encontrar o usuário pelo id
         let result = await Users.findByPk(id);
 
-        if (!result) { return res.status(400).send(`Usuario não foi encontrado!`) }
+        // Se o usuário não for encontrado, retornar erro
+        if (!result) {
+            return res.status(404).send(`Usuário não foi encontrado!`);
+        }
 
         try {
-            await Users.update(data, { where: { id } });
-        } catch (error) { return res.status(400).send(`Não foi possivel atulizar: ${error}`) };
+            await Users.update(dados, { where: { id } });
+        } catch (error) {
+            const msg = 'Erro ao tentar Gravar!';
+            const erro = error?.message;
+            return res.status(400).json({ msg, erro });
+        }
 
         result = await Users.findByPk(id);
-        return res.status(200).json({ msg: `GRAVADO COM SÚCESSO!`, dados: result.dataValues });
 
-    } catch (error) { return res.status(400).send(`Não foi possivel fazer o processo: ${error}`) }
+        // Retornar sucesso com os dados atualizados
+        return res.status(200).json({
+            msg: "GRAVADO COM SUCESSO!",
+            data: result.dataValues
+        });
+
+    } catch (error) {
+        return res.status(400).send(`Não foi possível fazer o processo: ${error.message}`);
+    }
 }
 
 module.exports = updateUser;
