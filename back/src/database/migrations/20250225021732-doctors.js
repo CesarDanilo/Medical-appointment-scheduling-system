047134b6b8e786aae69a30f0f4bfc0956e3c917b
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-24 23:24:07 -0300

    add id migrate

--- back/src/database/migrations/20250225021732-doctors.js
+++ back/src/database/migrations/20250225021732-doctors.js
@@ -2,21 +2,20 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    'doctors', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      }
+    }
+
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+
   }
 };
