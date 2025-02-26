341ae088fe6bb2b6707e80523f1070f39c89ece1
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-26 13:32:25 -0300

    adicionando rotas doctors

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routes = express.Router();
 const user_api = require("../api/users/user-api");
+const doctors_api = require("../api/doctors/doctors-api");
 
 routes.use('/user', user_api);
+routes.use('/doctors', doctors_api);
 module.exports = routes;
