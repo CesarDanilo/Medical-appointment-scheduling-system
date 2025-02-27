42779611734e29ec153f4f28b999f9d14162b3a2
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-26 21:42:52 -0300

    create models Patients

---
+++ back/src/database/models/Patients.js
@@ -0,0 +1,47 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Patients extends Model {
+        static associate(models) {
+            // Doctors.hasMany(models.Pedido, { foreignKey: 'usuario_id' });
+        }
+    }
+    Patients.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id'
+            }
+        },
+        date_for_birth: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        gender: {
+            type: DataTypes.STRING(250),
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
+        }
+    }, {
+        sequelize,
+        modelName: 'Patients',
+        tableName: 'patients',
+        timestamps: true // Habilita o gerenciamento automático de createdAt e updatedAt
+    });
+
+    return Patients;
+};
