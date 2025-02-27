3f1991a86897d09bf3a11064a72050d0bd600a6e
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-26 22:07:02 -0300

    configurando rotas

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const routes = express.Router();
 const user_api = require("../api/users/user-api");
 const doctors_api = require("../api/doctors/doctors-api");
+const patients_api = require("../api/patients/patients-api");
 
 routes.use('/user', user_api);
 routes.use('/doctors', doctors_api);
+routes.use('/patients', patients_api);
 module.exports = routes;
