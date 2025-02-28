4992af0e0aa72d61d82e2af607f38164acdea839
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-28 15:59:48 -0300

    created api Schedules

---
+++ back/src/api/schedules/schedules-api.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const routes = express.Router();
+
+const schedulesControllers = require('../../controllers/schedules');
+
+routes.get('/', schedulesControllers.readSchedules);
+routes.get('/:id', schedulesControllers.readSchedules);
+
+routes.post('/create-schedules', schedulesControllers.createSchedules);
+
+routes.put('/:id', schedulesControllers.updateSchedules);
+
+routes.delete('/:id', schedulesControllers.deleteSchedules);
+
+module.exports = routes;
+
+
