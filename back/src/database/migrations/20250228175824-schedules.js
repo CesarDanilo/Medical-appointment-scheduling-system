122592abb218a87265fe92168e3ae3ea2fb8e67c
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-02-28 14:59:52 -0300

    created migrate schadules

---
+++ back/src/database/migrations/20250228175824-schedules.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'schedules', {
+
+      id: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        primaryKey: true
+      },
+      user_id: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      date_for_birth: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      gender: {
+        type: Sequelize.STRING(250),
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('schedules');
+  }
+};
