b30a9cf7a4217d3971cc44f6156ee2c66f85ef81
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-06 15:19:44 -0300

    create api appointments_api

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -4,9 +4,12 @@ const user_api = require("../api/users/user-api");
 const doctors_api = require("../api/doctors/doctors-api");
 const patients_api = require("../api/patients/patients-api");
 const schedules_api = require("../api/schedules/schedules-api");
+const appointments_api = require("../api/appointments/appointments-api");
 
 routes.use('/user', user_api);
 routes.use('/doctors', doctors_api);
 routes.use('/patients', patients_api);
 routes.use('/schedules', schedules_api);
+routes.use('/appointments', appointments_api);
+
 module.exports = routes;
