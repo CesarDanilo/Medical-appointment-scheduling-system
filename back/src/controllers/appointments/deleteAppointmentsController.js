dac1ad07a7c33aed7691b82aaecbd95aaafce4a6
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-06 15:13:09 -0300

    criando delete

--- back/src/controllers/appointments/deleteAppointmentsController.js
+++ back/src/controllers/appointments/deleteAppointmentsController.js
@@ -1,19 +1,19 @@
-const { Doctors } = require("../../database/models");
+const { Appointments } = require("../../database/models");
 
-const deleteDoctors = async (req, res) => {
+const deleteAppointments = async (req, res) => {
     const { id } = req.params;
 
     try {
-        let result = await Doctors.findByPk(id);
+        let result = await Appointments.findByPk(id);
 
         if (!result) { return res.status(500).send(`Doctor não encontrado com id ${id}`); }
 
         try {
-            await Doctors.destroy({ where: { id } });
+            await Appointments.destroy({ where: { id } });
         } catch (error) { res.status(400).send(`Não foi possível deletar o Doctor: ${error}`) }
 
         return res.status(200).send(`Item deletado com súcesso! id: ${id}`);
     } catch (error) { res.status(400).send(`Não foi possível fazer a operação: ${error}`) }
 }
 
-module.exports = deleteDoctors;
+module.exports = deleteAppointments;
