7d8f44780962ea0ecad465fb6f762897fc5834bd
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-10 15:37:25 -0300

    adicionando campos para o migrate

--- back/src/database/migrations/20250310182746-payments.js
+++ back/src/database/migrations/20250310182746-payments.js
@@ -2,7 +2,51 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'payments', {
+
+      id: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        primaryKey: true
+      },
+      appointment_id: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      amout: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      payment_method: {
+        type: Sequelize.STRING(250),
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.STRING(250),
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      }
+    });
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('payments');
+
+  }
+};
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
@@ -11,7 +55,7 @@ module.exports = {
      */
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
