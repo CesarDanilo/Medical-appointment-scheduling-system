40e73dc415d6fc261b1b798dd19c96b3a2f1e349
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-13 15:52:49 -0300

    finalizando todos os testes de rotas do reminders

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -5,13 +5,13 @@ const doctors_api = require("../api/doctors/doctors-api");
 const patients_api = require("../api/patients/patients-api");
 const schedules_api = require("../api/schedules/schedules-api");
 const appointments_api = require("../api/appointments/appointments-api")
-const remainders_api = require("../api/reminders/reminders-api");
+const reminders_api = require("../api/reminders/reminders-api");
 
 routes.use('/user', user_api);
 routes.use('/doctors', doctors_api);
 routes.use('/patients', patients_api);
 routes.use('/schedules', schedules_api);
 routes.use('/appointments', appointments_api);
-routes.use('/reminders', remainders_api);
+routes.use('/reminders', reminders_api);
 
 module.exports = routes;
