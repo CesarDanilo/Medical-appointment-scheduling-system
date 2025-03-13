59af7a92ddcbf17a5f6fe76488bed31f57abfc24
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-13 15:36:32 -0300

    delete

--- back/src/controllers/reminders/deleteRemindersController.js
+++ back/src/controllers/reminders/deleteRemindersController.js
@@ -1,19 +1,19 @@
-const { Appointments } = require("../../database/models");
+const { Reminders } = require("../../database/models");
 
-const deleteAppointments = async (req, res) => {
+const deleteReminders = async (req, res) => {
     const { id } = req.params;
 
     try {
-        let result = await Appointments.findByPk(id);
+        let result = await Reminders.findByPk(id);
 
-        if (!result) { return res.status(500).send(`Doctor não encontrado com id ${id}`); }
+        if (!result) { return res.status(500).send(`Reminders não encontrado com id ${id}`); }
 
         try {
-            await Appointments.destroy({ where: { id } });
-        } catch (error) { res.status(400).send(`Não foi possível deletar o Doctor: ${error}`) }
+            await Reminders.destroy({ where: { id } });
+        } catch (error) { res.status(400).send(`Não foi possível deletar o Reminders: ${error}`) }
 
         return res.status(200).send(`Item deletado com súcesso! id: ${id}`);
     } catch (error) { res.status(400).send(`Não foi possível fazer a operação: ${error}`) }
 }
 
-module.exports = deleteAppointments;
+module.exports = deleteReminders;
