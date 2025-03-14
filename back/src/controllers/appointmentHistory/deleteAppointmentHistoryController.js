bbedf578956b754479f574aaed042982a926d9ee
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-14 15:30:57 -0300

    .

--- back/src/controllers/appointmentHistory/deleteAppointmentHistoryController.js
+++ back/src/controllers/appointmentHistory/deleteAppointmentHistoryController.js
@@ -1,19 +1,19 @@
-const { Schedules } = require("../../database/models");
+const { AppointmentHistory } = require("../../database/models");
 
-const deleteSchedules = async (req, res) => {
+const deleteAppointmentHistory = async (req, res) => {
     const { id } = req.params;
 
     try {
-        let result = await Schedules.findByPk(id);
+        let result = await AppointmentHistory.findByPk(id);
 
         if (!result) { return res.status(500).send(`Doctor não encontrado com id ${id}`); }
 
         try {
-            await Schedules.destroy({ where: { id } });
+            await AppointmentHistory.destroy({ where: { id } });
         } catch (error) { res.status(400).send(`Não foi possível deletar o Doctor: ${error}`) }
 
         return res.status(200).send(`Item deletado com súcesso! id: ${id}`);
     } catch (error) { res.status(400).send(`Não foi possível fazer a operação: ${error}`) }
 }
 
-module.exports = deleteSchedules;
+module.exports = deleteAppointmentHistory;
