37f24f5acace13d4e6249e00443bd6a9748e2825
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-14 15:33:23 -0300

    created read appointment history

--- back/src/controllers/appointmentHistory/readAppointmentHistoryController.js
+++ back/src/controllers/appointmentHistory/readAppointmentHistoryController.js
@@ -1,10 +1,10 @@
-const { Schedules } = require("../../database/models");  // Importa o modelo 'Schedules' para interagir com a tabela 'Schedules'
+const { AppointmentHistory } = require("../../database/models");  // Importa o modelo 'AppointmentHistory' para interagir com a tabela 'AppointmentHistory'
 const { Op } = require("sequelize");  // Importa o operador 'Op' do Sequelize para realizar operações de comparação
 
 // Função para buscar agendamentos de médicos com base em filtros
-const schedulesControllers = async (req, res) => {
+const AppointmentHistoryControllers = async (req, res) => {
     try {
-        const { id, doc_id } = req.query;  // Desestrutura os parâmetros 'id' e 'doc_id' da query string da requisição
+        const { id } = req.query;  // Desestrutura os parâmetros 'id' e 'doc_id' da query string da requisição
         const where = {};  // Cria um objeto 'where' vazio que será usado para armazenar os filtros de busca
 
         // Verifica se foi passado o parâmetro 'id' na query e adiciona ao filtro 'where'
@@ -14,17 +14,10 @@ const schedulesControllers = async (req, res) => {
             };
         }
 
-        // Verifica se foi passado o parâmetro 'doc_id' (doctor_id) na query e adiciona ao filtro 'where'
-        if (doc_id) {
-            where.doctor_id = {
-                [Op.eq]: doc_id  // O operador 'Op.eq' compara se o 'doctor_id' é igual ao valor fornecido
-            };
-        }
-
         // Realiza a consulta no banco de dados usando os filtros de 'where' criados
-        const result = await Schedules.findAll({
+        const result = await AppointmentHistory.findAll({
             where,  // Aplica os filtros de busca
-            attributes: ["id", "doctor_id", "start_time", "end_time", "is_available", "createdAt", "updatedAt"]  // Define quais atributos (colunas) do agendamento devem ser retornados
+            attributes: ["id", "appointment_id", "diagnosis", "prescription", "createdAt", "updatedAt"]  // Define quais atributos (colunas) do agendamento devem ser retornados
         });
 
         // Retorna os resultados encontrados com status 200
@@ -35,4 +28,4 @@ const schedulesControllers = async (req, res) => {
     }
 };
 
-module.exports = schedulesControllers;  // Exporta a função para ser usada em outros lugares
+module.exports = AppointmentHistoryControllers;  // Exporta a função para ser usada em outros lugares
