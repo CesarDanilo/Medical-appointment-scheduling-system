ca5e1db54a6755df330a8b7d07da437b021d7b85
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-03-20 15:12:52 -0300

    mudndo de porta de 3000 para 3001

--- back/src/server.js
+++ back/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('./database/config/db');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 const routes = require("./routes/routes");
