b330cebd3776cca14665d7e818142ba01d3ecfc2
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-04-03 15:59:51 -0300

    implementando formulario

--- front/src/examples/Forms/FormUsers/index.jsx
+++ front/src/examples/Forms/FormUsers/index.jsx
@@ -4,7 +4,43 @@ import VuiInput from "components/VuiInput";
 
 const FormUsers = () => {
     return (
-        <VuiBox sx={{ width: "100%", maxWidth: "800px", mx: "auto" }}>
+        <VuiBox sx={{ width: "100%", maxWidth: "800px", mx: "auto", display: "flex", flexDirection: "row"}}>
+            <VuiBox sx={{ width: "100%", display: "flex", flexDirection: "column", gap: 2 }}>
+                <VuiBox>
+                    <VuiTypography variant="body2" color="info" textGradient mb={1}>
+                        Nome
+                    </VuiTypography>
+                    <VuiInput
+                        placeholder="Digite o nome completo..."
+                        size="medium"
+                        fullWidth
+                        sx={{
+                            backgroundColor: "transparent",
+                            "& .MuiInputBase-input": {
+                                py: 1.5,
+                                fontSize: "0.875rem"
+                            }
+                        }}
+                    />
+                </VuiBox>
+
+                <VuiBox>
+                    <VuiTypography variant="body2" color="info" textGradient mb={1}>
+                        Email
+                    </VuiTypography>
+                    <VuiInput
+                        placeholder="Digite o email..."
+                        fullWidth
+                        sx={{
+                            backgroundColor: "transparent",
+                            "& .MuiInputBase-input": {
+                                py: 1.5,
+                                fontSize: "0.875rem"
+                            }
+                        }}
+                    />
+                </VuiBox>
+            </VuiBox>
             <VuiBox sx={{ width: "100%", display: "flex", flexDirection: "column", gap: 2 }}>
                 <VuiBox>
                     <VuiTypography variant="body2" color="info" textGradient mb={1}>
@@ -44,5 +80,5 @@ const FormUsers = () => {
         </VuiBox>
     )
 }
-
+    
 export default FormUsers;   
