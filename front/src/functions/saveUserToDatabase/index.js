c7031073aae54f6d42e3c7ca2796c03a949d78f2
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2025-04-07 15:32:35 -0300

    gravando dados no banco de dados

--- front/src/functions/saveUserToDatabase/index.js
+++ front/src/functions/saveUserToDatabase/index.js
@@ -4,9 +4,10 @@ const saveUserToDatabase = async (data) => {
     const url = "http://localhost:3001/user/create-user"
     try {
         const response = await axios.post(url, data);
+        return response.status
 
     } catch (error) {
-
+        return error;
     }
 }
 
